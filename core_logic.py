941e77c0514dbc08611aff30979641b4d15d8e86
TalhaSMinhas <talha.salman.minhas@icloud.com>
2025-08-31 23:58:56 +0200

    porcessing complete

--- core_logic.py
+++ core_logic.py
@@ -5,6 +5,33 @@ DPI = 400
 USE_PNG = True
 JPEG_QUALITY = 95
 
+def process(input_path: Path, output_path: Path):
+    pdf_dir = Path(input_path)  # change this
+    out_dir = Path(output_path)
+    out_dir.mkdir(exist_ok=True)
+
+    zoom = DPI / 72.0
+    mat = fitz.Matrix(zoom, zoom)
+
+    for pdf_path in list(pdf_dir.glob("*.pdf")) + list(pdf_dir.glob("*.PDF")):
+        print(f"Processing {pdf_path.name} ...")
+        try:
+            with fitz.open(pdf_path) as doc:
+                pages_done = 0
+                for i, page in enumerate(doc, start=1):
+                    pix = page.get_pixmap(matrix=mat, alpha=False)
+                    if USE_PNG:
+                        pix.save(out_dir / f"{pdf_path.stem}-page-{i}.png")
+                    else:
+                        pix.save(out_dir / f"{pdf_path.stem}-page-{i}.jpg", quality=JPEG_QUALITY)
+                    pages_done += 1
+
+            print(f"✔ Saved {pages_done} pages from {pdf_path.name}")
+        except Exception as e:
+            print(f"⚠️ Skipped {pdf_path.name}: {e}")
+
+    print("✅ All PDFs processed. Images are in:", out_dir.resolve())
+
 def receive_paths(input_path: Path, output_path: Path):
     input_path = Path(input_path).expanduser().resolve()
     output_path = Path(output_path).expanduser().resolve()
@@ -14,39 +41,9 @@ def receive_paths(input_path: Path, output_path: Path):
     if input_path.is_file():
         raise NotADirectoryError(f"Expected a folder, got a file: {input_path}")
 
-    output_path.mkdir(parents=True, exist_ok=True)
+    process(input_path, output_path)
 
-    return [input_path, output_path]
-
-
-'''
-pdf_dir = Path()   # change this
-out_dir = Path("exported_pages_multi")
-out_dir.mkdir(exist_ok=True)
-
-zoom = DPI / 72.0
-mat = fitz.Matrix(zoom, zoom)
-
-for pdf_path in list(pdf_dir.glob("*.pdf")) + list(pdf_dir.glob("*.PDF")):
-    print(f"Processing {pdf_path.name} ...")
-    try:
-        with fitz.open(pdf_path) as doc:
-            pages_done = 0
-            for i, page in enumerate(doc, start=1):
-                pix = page.get_pixmap(matrix=mat, alpha=False)
-                if USE_PNG:
-                    pix.save(out_dir / f"{pdf_path.stem}-page-{i}.png")
-                else:
-                    pix.save(out_dir / f"{pdf_path.stem}-page-{i}.jpg", quality=JPEG_QUALITY)
-                pages_done += 1
-
-        print(f"✔ Saved {pages_done} pages from {pdf_path.name}")
-    except Exception as e:
-        print(f"⚠️ Skipped {pdf_path.name}: {e}")
-
-print("✅ All PDFs processed. Images are in:", out_dir.resolve())
-
-'''
+    return 0
 
 if __name__ == "__main__":
     i, o = receive_paths(Path.cwd(), Path.cwd())
